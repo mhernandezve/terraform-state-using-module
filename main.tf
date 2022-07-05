4c645557d3329882752923afad6ae19ec8b13c87
Miguel Hernández <mh@andsoluciones.com>
2022-07-05 14:54:12 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket  = "tfstate-mhg"
+    key     = "modules/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+module "storage-mod" {
+  source       = "./modules/storage"
+  bucket-name  = "mhg-storage-mod"
+  region       = "us-east-1"
+  acl          = "public-read"
+  allow_access = ["arn:aws:iam::112830045092:user/miguel.globant"]
+  versioned    = "Enabled"
+}
+
+
---
+++ modules/storage/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "bucket-name" {
+  bucket   = var.bucket-name
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket-name.id
+  acl    = var.acl
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket-name.id
+  versioning_configuration {
+    status = var.versioned
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access" {
+  bucket = aws_s3_bucket.bucket-name.id 
+  policy = data.aws_iam_policy_document.allow_access_document.json
+}
+
+data "aws_iam_policy_document" "allow_access_document" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = var.allow_access
+    }
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = ["arn:aws:s3:::${var.bucket-name}"]
+ }
+}
